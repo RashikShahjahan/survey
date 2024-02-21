ccf953b857cf46b0867a0f3486f52b1345cffc21
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-02-20 23:24:40 -0500

    test

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
-import uvicorn
+#!/usr/bin/env python3
 
-app = FastAPI()
+from flask import Flask, request
 
-@app.get("/print/{text}")
-async def read_item(text: str):
-    return {"text": text}
+app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return '''
+     <form action="/echo_user_input" method="POST">
+         <input name="user_input">
+         <input type="submit" value="Submit!">
+     </form>
+     '''
+
+@app.route("/echo_user_input", methods=["POST"])
+def echo_input():
+    input_text = request.form.get("user_input", "")
+    return "You entered: " + input_text
