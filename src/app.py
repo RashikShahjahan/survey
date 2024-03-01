e84ec2d8915b0d37d126e1374921f5a3d90210e5
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-03-01 17:24:02 -0500

    ss

--- src/app.py
+++ src/app.py
@@ -1,19 +1,45 @@
-#!/usr/bin/env python3
-
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template_string
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# Configure the SQLAlchemy part
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///survey.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route("/")
-def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+# Define your database model
+class Survey(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    feedback = db.Column(db.String(250), nullable=False)
 
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+# Create the database
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+# Serve the survey form
+@app.route('/')
+def survey():
+    form_html = '''
+    <form action="/submit" method="post">
+        <label for="feedback">Your feedback:</label><br>
+        <input type="text" id="feedback" name="feedback"><br>
+        <input type="submit" value="Submit">
+    </form> 
+    '''
+    return render_template_string(form_html)
+
+# Handle form submission
+@app.route('/submit', methods=['POST'])
+def submit_survey():
+    feedback = request.form['feedback']
+    new_survey = Survey(feedback=feedback)
+    db.session.add(new_survey)
+    db.session.commit()
+    return redirect('/count')
+
+# Count and display the number of surveys
+@app.route('/count')
+def count_surveys():
+    total_surveys = Survey.query.count()
+    return f'Total number of surveys submitted: {total_surveys}'
